ee62e6e03f64feb52d994165644fa67cb195dacc
Alexandre MOTTET <alexandre.mottet@idfor.fr>
2018-05-07 15:10:18 +0200

    Add article detail

---
+++ blog/src/templates/article.js
@@ -0,0 +1,21 @@
+import React from 'react'  
+import Link from 'gatsby-link'
+
+const ArticleTemplate = ({ data }) => (  
+  <div>
+    <h1>{data.strapiArticle.title}</h1>
+    <p>by</p>
+    <p>{data.strapiArticle.content}</p>
+  </div>
+)
+
+export default ArticleTemplate
+
+export const query = graphql`  
+  query ArticleTemplate($id: String!) {
+    strapiArticle(id: {eq: $id}) {
+      title
+      content
+    }
+  }
+`
