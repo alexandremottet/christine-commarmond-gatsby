69ad3710c3228b5728dcb03c3b33e7a4d511dbb0
Alexandre MOTTET <alexandre.mottet@idfor.fr>
2018-05-15 17:57:37 +0200

    Change ui

--- src/components/header.js
+++ src/components/header.js
@@ -4,14 +4,14 @@ import Link from 'gatsby-link'
 import './header.css'
 
 const Header = ({ siteTitle }) => (
-  <div className="ui menu">
+  <header className="ui menu">
     <Link className="item" to="/">
       Accueil
     </Link>
     <Link className="item" to="/articles/">
       Blog
     </Link>
-  </div>
+  </header>
 )
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import './index.css'
+
 const IndexPage = ({ data }) => (
   <div>
-    <h1>christine commarmond</h1>
+    <section id="cover">
+        <div>
+            <h1>christine commarmond</h1>
+        </div>
+        <div style={{postion: 'relative'}}>
+            <span className="scroll"></span>
+        </div>
+    </section>
   </div>
 )
 
