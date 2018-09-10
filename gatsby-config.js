489d1bcd4f05e84fa354f2f91599f9a755059e67
Alexandre MOTTET <mottetalexandre@gmail.com>
2018-09-10 16:01:38 +0200

    change site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Christine Commarmond - Ecrivaine Publique`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
