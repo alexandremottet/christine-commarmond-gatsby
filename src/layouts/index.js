b9ec0c7bf4102f3354bb6d9e3c9fb8448eb649a5
Alexandre MOTTET <mottetalexandre@gmail.com>
2018-09-12 11:36:55 +0200

    Add SEO tags

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -9,11 +9,6 @@ const Layout = ({ children, data }) => (
   <div id="content">
     <Helmet
       title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: `Site personnel de Christine Commarmond écrivaine Publique dans la région Auvergne` },
-        { name: 'keywords', content: `écrivain, écrivaine, publique, christine, commarmond, auvergne, puy-de-dome` },
-        { name: 'robots', content: `index, follow`},
-      ]}
       link={[
         { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/semantic-ui@2.3.1/dist/semantic.min.css' },
         { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.3.1/css/all.css', integrity: 'sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU', crossorigin: 'anonymous' }
@@ -22,7 +17,17 @@ const Layout = ({ children, data }) => (
         { src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js' },
         { src: 'https://cdn.jsdelivr.net/npm/semantic-ui@2.3.1/dist/semantic.min.js' }
       ]}
-    />
+    >
+      <meta property="title" content={data.site.siteMetadata.title} />
+      <meta property="description" content='Site personnel de Christine Commarmond écrivaine Publique dans la région Auvergne' />
+      <meta property="keywords" content='écrivain, écrivaine, publique, christine, commarmond, auvergne, puy-de-dome' />
+      <meta property="robots" content='index, follow' />
+      <meta property="title" content={data.site.siteMetadata.title} />
+      <meta property="author" content='Christine Commarmond' />
+      <meta property="og:title" content={data.site.siteMetadata.title} />
+      <meta property="og:description" content='Site personnel de Christine Commarmond écrivaine Publique dans la région Auvergne' />
+      <meta property="og:type" content='website' />
+    </Helmet>
     <Header siteTitle={data.site.siteMetadata.title} />
     <div style={{height:'50px'}}></div>
     {children()}
