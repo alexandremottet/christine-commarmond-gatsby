78820b5949c04463ce4af2e75bdcb6a6ca38a7be
Alexandre MOTTET <mottetalexandre@gmail.com>
2018-09-13 19:10:28 +0200

    SEO

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,16 +22,21 @@ const Layout = ({ children }) => (
       <>
         <Helmet
           title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
           link={[
             { rel: 'stylesheet', href: '//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css' },
             { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.3.1/css/all.css', integrity: 'sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU', crossorigin: 'anonymous' }
           ]}
         >
           <html lang="en" />
+          <meta property="title" content={data.site.siteMetadata.title} />
+          <meta property="description" content='Site personnel de Christine Commarmond écrivaine Publique dans la région Auvergne' />
+          <meta property="keywords" content='écrivain, écrivaine, publique, public, christine, commarmond, auvergne, puy-de-dome, sainte-florine' />
+          <meta property="robots" content='index, follow' />
+          <meta property="title" content={data.site.siteMetadata.title} />
+          <meta property="author" content='Christine Commarmond' />
+          <meta property="og:title" content={data.site.siteMetadata.title} />
+          <meta property="og:description" content='Site personnel de Christine Commarmond écrivaine Publique dans la région Auvergne' />
+          <meta property="og:type" content='website' />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
         {children}
