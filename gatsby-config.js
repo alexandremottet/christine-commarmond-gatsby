e4c22f9704af89051a6d299e3876b443fa96ea55
Alexandre MOTTET <alexandremottet.dev@gmail.com>
2019-06-16 19:54:36 +0200

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,47 +1,57 @@
 module.exports = {
   siteMetadata: {
-    title: 'Christine Commarmond - Ecrivaine Publique',
-    siteUrl: 'https://christinecommarmond.fr'
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-offline',
-    'gatsby-plugin-sitemap',
+    `gatsby-plugin-react-helmet`,
     {
-      resolve: 'gatsby-plugin-robots-txt',
+      resolve: `gatsby-source-filesystem`,
       options: {
-        host: 'https://christinecommarmond.fr',
-        sitemap: 'https://christinecommarmond.fr/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }]
-      }
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
     },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "https://christinecommarmond.fr",
+        sitemap: "https://christinecommarmond.fr/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
       },
     },
     {
-      resolve: 'gatsby-source-strapi',
+      resolve: "gatsby-source-strapi",
       options: {
         apiURL: `http://localhost:1337`,
-        contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
+        contentTypes: [
+          // List of the Content Types you want to be able to request from Gatsby.
           `presentation`,
           `article`,
           `news`,
           `activity`,
-          `user`
-        ]
+          `user`,
+        ],
       },
     },
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: "UA-125509832-1",
         head: false,
@@ -49,8 +59,11 @@ module.exports = {
         respectDNT: true,
         exclude: ["/preview/**", "/do-not-track/me/too/"],
         sampleRate: 5,
-        siteSpeedSampleRate: 10
+        siteSpeedSampleRate: 10,
       },
-    }
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
 }
