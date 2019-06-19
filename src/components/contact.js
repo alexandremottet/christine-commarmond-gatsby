285d955d59c15977559c89b707a3371d44989a01
Alexandre MOTTET <alexandre.mottet@allegorithmic.com>
2019-06-19 14:11:07 +0200

    Disable contact button if the form is not fill

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,83 +1,130 @@
 import React from "react"
-import Reaptcha from 'reaptcha';
-import axios from 'axios';
-import { ToastContainer, toast } from 'react-toastify';
+import Reaptcha from "reaptcha"
+import axios from "axios"
+import { ToastContainer, toast } from "react-toastify"
 
 class Contact extends React.Component {
-
   constructor(props) {
-    super(props);
-    this.captcha = null;
+    super(props)
+    this.captcha = null
+    this.form = null
     this.state = {
-        name: '',
-        email: '',
-        subject: '',
-        body: ''
+      name: "",
+      email: "",
+      subject: "",
+      body: "",
     }
 
-    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this)
   }
 
-  onVerify = (response) => {
-    console.log('onVerify', this.state.name);
-    axios.post('https://us-central1-christine-commarmond.cloudfunctions.net/email/', {
-        name: this.state.name,
-        email: this.state.email,
-        subject: this.state.subject,
-        body: this.state.body
-    })
-    .then(function (response) {
-      toast.success("Votre message a été envoyé", {
-        position: toast.POSITION.TOP_CENTER
-      });
-    })
-    .catch(function (error) {
-      toast.error("Votre message n'a pu être envoyé", {
-        position: toast.POSITION.TOP_LEFT
-      });
-    });
-  };
+  onVerify = token => {
+    axios
+      .post(
+        "https://us-central1-christine-commarmond.cloudfunctions.net/email/",
+        {
+          name: this.state.name,
+          email: this.state.email,
+          subject: this.state.subject,
+          body: this.state.body,
+          token: token,
+        }
+      )
+      .then(() => {
+        toast.success("Votre message a été envoyé", {
+          position: toast.POSITION.TOP_CENTER,
+        })
+      })
+      .catch(error => {
+        toast.error("Votre message n'a pu être envoyé", {
+          position: toast.POSITION.TOP_LEFT,
+        })
+      })
+      .finally(() => {
+        this.captcha.reset()
+      })
+  }
 
-  handleInputChange = (event) => {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
+  handleInputChange = event => {
+    const target = event.target
+    const value = target.type === "checkbox" ? target.checked : target.value
+    const name = target.name
 
     this.setState({
-      [name]: value
-    });
+      [name]: value,
+    })
   }
 
   onExecute = () => {
-    this.captcha.execute();
-  };
+    this.captcha.execute()
+  }
 
   render() {
+    const disabled = this.form ? !this.form.checkValidity() : true
     return (
       <section id="contact">
-        <div className="ui centered grid" style={{margin:0, paddingTop: '50px'}}>
+        <div
+          className="ui centered grid"
+          style={{ margin: 0, paddingTop: "50px" }}
+        >
           <div className="ten wide column">
             <h1>Contact</h1>
-            <ToastContainer className='toast-container' toastClassName="toast-body"/>
-            <form className="ui form" action="">
+            <ToastContainer
+              className="toast-container"
+              toastClassName="toast-body"
+            />
+            <form className="ui form" action="" ref={e => (this.form = e)}>
               <div className="field">
-                <label>Nom</label>
-                <input type="text" name="name" placeholder="Votre nom" value={this.state.name} onChange={this.handleInputChange} required />
+                <label>Nom *</label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Votre nom"
+                  value={this.state.name}
+                  onChange={this.handleInputChange}
+                  required
+                />
               </div>
               <div className="field">
-                <label>Email</label>
-                <input type="email" name="email" placeholder="Votre email" value={this.state.email} onChange={this.handleInputChange} required />
+                <label>Email *</label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Votre email"
+                  value={this.state.email}
+                  onChange={this.handleInputChange}
+                  required
+                />
               </div>
               <div className="field">
-                <label>Sujet</label>
-                <input type="text" name="subject" placeholder="Le sujet de votre message" value={this.state.subject} onChange={this.handleInputChange} required />
+                <label>Sujet *</label>
+                <input
+                  type="text"
+                  name="subject"
+                  placeholder="Le sujet de votre message"
+                  value={this.state.subject}
+                  onChange={this.handleInputChange}
+                  required
+                />
               </div>
               <div className="field">
-                <label>Votre message</label>
-                <textarea name="body" value={this.state.body} onChange={this.handleInputChange} required />
+                <label>Votre message *</label>
+                <textarea
+                  name="body"
+                  value={this.state.body}
+                  onChange={this.handleInputChange}
+                  required
+                />
               </div>
             </form>
-            <button className="ui button" type="submit" onClick={this.onExecute}>Envoyer</button>
+            <button
+              className="ui button"
+              type="submit"
+              onClick={this.onExecute}
+              disabled={disabled}
+            >
+              Envoyer
+            </button>
             <div className="badge-container">
               <Reaptcha
                 ref={e => (this.captcha = e)}
